3a81a9f7bb8d88b40367d02b25559c744ad31726
Suhas Bacchu <suhas.bacchu29@gmail.com>
2019-12-02 14:58:31 -0600

    Added a HTML Page for demo

--- converted_keras/AI_Test.py
+++ converted_keras/AI_Test.py
@@ -1,4 +1,5 @@
 import tensorflow.keras
+import cv2
 from PIL import Image
 import numpy as np
 
@@ -7,25 +8,55 @@ np.set_printoptions(suppress=True)
 
 # Load the model
 model = tensorflow.keras.models.load_model('keras_model.h5')
+# Capture the video
+video = cv2.VideoCapture(0)
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+# # Create the array of the right shape to feed into the keras model
+# # The 'length' or number of images you can put into the array is
+# # determined by the first position in the shape tuple, in this case 1.
+# data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# Replace this with the path to your image
-image = Image.open('Path to your image')
+# # Replace this with the path to your image
+# image = Image.open('Path to your image')
 
-# Make sure to resize all images to 224, 224 otherwise they won't fit in the array
-image = image.resize((224, 224))
-image_array = np.asarray(image)
+# # Make sure to resize all images to 224, 224 otherwise they won't fit in the array
+# image = image.resize((224, 224))
+# image_array = np.asarray(image)
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+# # Normalize the image
+# normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
-# Load the image into the array
-data[0] = normalized_image_array
+# # Load the image into the array
+# data[0] = normalized_image_array
 
-# run the inference
-prediction = model.predict(data)
-print(prediction)
+# # run the inference
+# prediction = model.predict(data)
+# print(prediction)
+
+while True:
+        _, frame = video.read()
+
+        #Convert the captured frame into RGB
+        im = Image.fromarray(frame, 'RGB')
+
+        #Resizing into 128x128 because we trained the model with this image size.
+        im = im.resize((128,128))
+        img_array = np.array(im)
+
+        #Our keras model used a 4D tensor, (images x height x width x channel)
+        #So changing dimension 128x128x3 into 1x128x128x3 
+        img_array = np.expand_dims(img_array, axis=0)
+
+        #Calling the predict method on model to predict 'me' on the image
+        prediction = int(model.predict(img_array)[0][0])
+
+        #if prediction is 0, which means I am missing on the image, then show the frame in gray color.
+        if prediction == 0:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow("Capturing", frame)
+        key=cv2.waitKey(1)
+        if key == ord('q'):
+                break
+video.release()
+cv2.destroyAllWindows()
